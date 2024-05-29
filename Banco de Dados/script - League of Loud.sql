4f2e0b6b30e178a3317f5103d7ac024a08626da9
AnilmarChoque <anilmarorellana@gmail.com>
2024-05-29 12:20:08 -0300

    Adição do campo 'foto' na tabela do usuario

--- Banco de Dados/script - League of Loud.sql
+++ Banco de Dados/script - League of Loud.sql
@@ -6,11 +6,11 @@ CREATE TABLE usuario (
 	idUsuario INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(100),
     email VARCHAR(100),
-    senha VARCHAR(45)
+    senha VARCHAR(45),
+    foto varchar(200)
 );
 
-select * from usuario;
-SELECT idUsuario, nome, email, senha FROM usuario WHERE email = 'anilmar@gmail.com' AND senha = 'F4m1l145!';
+SELECT * FROM usuario;
 
 CREATE TABLE equipe (
 	idEquipe INT PRIMARY KEY AUTO_INCREMENT,
@@ -19,6 +19,8 @@ CREATE TABLE equipe (
     colocacao INT
 );
 
+SELECT * FROM equipe;
+
 CREATE TABLE jogador (
 	idJogador INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(45),
@@ -26,10 +28,59 @@ CREATE TABLE jogador (
     fkEquipe INT,
     FOREIGN KEY(fkEquipe) REFERENCES equipe(idEquipe)
 );
+	
+SELECT * FROM jogador;
 
 CREATE TABLE campeonato (
 	idCampeonato INT PRIMARY KEY AUTO_INCREMENT,
     nome VARCHAR(45),
     dataInicio DATETIME,
-   campeao VARCHAR(45)
+	campeao VARCHAR(45)
 );
+
+SELECT * FROM campeonato;
+
+CREATE TABLE avaliacao (
+	idAvaliacao INT AUTO_INCREMENT,
+    fkUsuario INT,
+    fkJogador INT,
+    primeiraNota INT,
+    segundaNota INT,
+    terceiraNota INT,
+    FOREIGN KEY (fkUsuario) REFERENCES usuario(idUsuario),
+    FOREIGN KEY (fkJogador) REFERENCES jogador(idJogador),
+    PRIMARY KEY (idAvaliacao, fkUsuario, fkJogador)
+);
+
+SELECT * FROM avaliacao;
+
+CREATE TABLE partida (
+	idPartida INT AUTO_INCREMENT,
+    fkEquipe INT,
+    fkCampeonato INT,
+    data DATETIME,
+    placar VARCHAR(45),
+	FOREIGN KEY (fkEquipe) REFERENCES equipe(idEquipe),
+    FOREIGN KEY (fkCampeonato) REFERENCES campeonato(idCampeonato),
+    PRIMARY KEY (idPartida, fkEquipe, fkCampeonato)
+);
+
+SELECT * FROM partida;
+
+CREATE TABLE performace (
+	idPerformace INT,
+    AMA VARCHAR(10),
+    dano INT,
+    ward VARCHAR(6),
+    fkJogador INT,
+    fkPartida INT,
+    fkEquipe INT,
+    fkCampeonato INT,
+    FOREIGN KEY (fkJogador) REFERENCES jogador(idJogador),
+    FOREIGN KEY (fkPartida) REFERENCES partida(idPartida),
+    FOREIGN KEY (fkEquipe) REFERENCES equipe(idEquipe),
+    FOREIGN KEY (fkCampeonato) REFERENCES campeonato(idCampeonato),
+    PRIMARY KEY (idPerformace, fkJogador)
+);
+
+SELECT * FROM performace;
